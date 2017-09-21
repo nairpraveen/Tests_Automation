7c60064285b2b019e41694f97dd9c9f20c6b23e7
AbhishikthaReddy <krabhishiktha@gmail.com>
2017-09-21 12:35:02 -0400

    FIX:changes-column order output

--- features/steps/step_output_files.py
+++ features/steps/step_output_files.py
@@ -3,11 +3,9 @@ from hamcrest    import assert_that, equal_to
 from files import retrieve_files
 from transformation import scenario
 from dir_file import dir_create
-# from result_files import files_create
 
 
-
-@given('the file')
+@given('a file')
 def step_given_the_file(context):
 	date = context.config.userdata.get("date")
 	masterfile_loc = context.config.userdata.get("masterfile_loc")
@@ -15,12 +13,9 @@ def step_given_the_file(context):
 	context.transformation = scenario()
 	datafiles_names, deffiles_names, control_data_file, control_def_file_loc = context.files.files(date, masterfile_loc)
 	assert_that(len(datafiles_names) > 0)
-# @when('the job checks for valid columns')
-# def step_when_file_generated(context):
-#     pass
 
 
-@then('rows count should match')
+@then('row count should match')
 def step_rows_count_should_match(context):
 	dir_file = dir_create()
 	values = dir_file.dir()
@@ -28,8 +23,6 @@ def step_rows_count_should_match(context):
 	masterfile_loc = context.config.userdata.get("masterfile_loc")
 	datafiles_names, deffiles_names, control_data_file, control_def_file_loc = context.files.files(date, masterfile_loc)
 	context.transformation.scenario_writing_to_files(datafiles_names, deffiles_names, control_data_file, control_def_file_loc)
-	# result_files = 
-	# assert_that("pass", equal_to(rows_count))
 
 
 @then('column names should match')
@@ -38,7 +31,6 @@ def step_column_names_should_match(context):
 	masterfile_loc = context.config.userdata.get("masterfile_loc")
 	datafiles_names, deffiles_names, control_data_file, control_def_file_loc = context.files.files(date, masterfile_loc)
 	context.transformation.scenario_writing_to_files(datafiles_names, deffiles_names, control_data_file, control_def_file_loc)
-	# assert_that("pass", equal_to(column_names))
 
 
 @then('column order should match')
@@ -47,4 +39,3 @@ def step_column_order_should_match(context):
 	masterfile_loc = context.config.userdata.get("masterfile_loc")
 	datafiles_names, deffiles_names, control_data_file, control_def_file_loc = context.files.files(date, masterfile_loc)
 	context.transformation.scenario_writing_to_files(datafiles_names, deffiles_names, control_data_file, control_def_file_loc)
-	# assert_that("pass", equal_to(column_order))
