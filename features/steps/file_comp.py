942887a20d7ceb1c7c9c5d0ea93ace7ee4f1bade
AbhishikthaReddy <krabhishiktha@gmail.com>
2017-09-28 10:20:29 -0400

    result display changed

--- features/steps/file_comp.py
+++ features/steps/file_comp.py
@@ -13,17 +13,23 @@ class f_comp(object):
 
 
 	def comp(self, text_file_summary_result, resultsfilelocation):
+
 		final_result, result = {}, {}
 		my_file = Path(text_file_summary_result)
 		if my_file.exists():
+
 			file_list = sorted(os.listdir(resultsfilelocation+"Summary_Result"+"/"), key=os.path.getctime)
+
 			if len(file_list) > 1:
+
 				with open(resultsfilelocation+"Summary_Result"+"/"+file_list[len(file_list)-1]+"/"+"summary_result.json", 'r') as datafile:
 					latest_data = json.load(datafile)
 				latest_data = pd.DataFrame(latest_data)
+
 				with open(resultsfilelocation+"Summary_Result"+"/"+file_list[len(file_list)-2]+"/"+"summary_result.json", 'r') as datafile:
 					previous_latest_data = json.load(datafile)
 				previous_latest_data = pd.DataFrame(previous_latest_data)
+
 				for i in range(0, len(latest_data.ix[1].index)):
 					for j in range(0, len(previous_latest_data.ix[1].index)):
 						# if latest_data.ix[1].index[i] == previous_latest_data.ix[1].index[j] and latest_data.ix[1][i] == "Pass" and previous_latest_data.ix[1][i] == "Pass":
@@ -34,11 +40,13 @@ class f_comp(object):
 							line = filecmp.cmp(resultsfilelocation+file_list[len(file_list)-1]+"/"+"Failed"+"/"+latest_data.ix[1].index[i], resultsfilelocation+file_list[len(file_list)-2]+"/"+"Pass"+"/"+previous_latest_data.ix[1].index[i])
 							if line == "False":
 								line = ["The file is different from the Previous created file"]
+							else:
+								line = ["The file has no conflicts"]
 						else:
-							line = ["The File has no conflicts"]
+							line = ["The File has no conflicts because it's a newly created file"]
 
 						result[latest_data.ix[1].index[i]] = line
 					final_result[file_list[len(file_list)-1]] = result
-				with open("C:/Users/Abhi/Documents/Orion/Tests_Automation/SUMMARY_FILE.json", 'a') as f2:
+				with open(resultsfilelocation+"SUMMARY_FILE.json", 'a') as f2:
 					json.dump(final_result, f2, indent=4)
 				f2.close()
--- features/steps/transformation.py
+++ features/steps/transformation.py
@@ -40,101 +40,108 @@ class scenario(object):
 		total_Summary_line = {}
 
 		for i in range(0, len(datafiles_names)):
+
 			client_file = datafiles_names[i]
 			json_def = deffiles_names[i]
 			sep_value = scenario.seperator_value(json_def)
-			client_file_data = pd.read_csv(client_file, sep= sep_value)
-			json_def_data = pd.read_json(json_def)
 			client_file_name = client_file.split('/')[2]
 			json_def_name = json_def.split('/')[1]
-
 			client_file_name_split = client_file_name[-len(client_file_name):-4]
 
 			text_file_pass = resultsfilelocation+today_now+"/"+"Pass/"
 			text_file_fail = resultsfilelocation+today_now+"/"+"Failed/"
 			text_file_result = resultsfilelocation+today_now+"/"+"Result/"+client_file_name_split+".json"
-			
-			# row count validation
 
-			# if int(control_file.xs(client_file_name)) != int(len(client_file_data)):
-			# 	line1 = {"Test": "Rows count", "Result": "Failed", "Output": "The partner file has "+str(int(control_file.xs(client_file_name)))+" rows but control file has "+str(int(len(client_file_data)))}
-			# else:
-			# 	line1 = {"Test": "Rows count", "Result": "Passed"}
+			if client_file_name in list(control_file.index):
 
-			#checking null values
+				client_file_data = pd.read_csv(client_file, sep= sep_value)
+				json_def_data = pd.read_json(json_def)
+				
+				# row count validation
 
-			l4 = client_file_data
-			dff = pd.DataFrame(l4)
-			dff2=dff[dff.isnull().any(axis=1)]
-			if(dff.isnull().sum().sum()):
-				dff2.index=dff2.index+1
-				output1= dff.columns[dff.isnull().any().tolist()] +":"+ str(dff2.index.tolist())
-				line1 = {"Test name": "Check for nulls", "Result": "Failed/Nulls are found","Null values found in":output1.tolist()}
+				# if int(control_file.xs(client_file_name)) != int(len(client_file_data)):
+				# 	line1 = {"Test": "Rows count", "Result": "Failed", "Output": "The partner file has "+str(int(control_file.xs(client_file_name)))+" rows but control file has "+str(int(len(client_file_data)))}
+				# else:
+				# 	line1 = {"Test": "Rows count", "Result": "Passed"}
+
+				#checking null values
+
+				l4 = client_file_data
+				dff = pd.DataFrame(l4)
+				dff2=dff[dff.isnull().any(axis=1)]
+				if(dff.isnull().sum().sum()):
+					dff2.index=dff2.index+1
+					output1= dff.columns[dff.isnull().any().tolist()] +":"+ str(dff2.index.tolist())
+					line1 = {"Test name": "Check for nulls", "Result": "Failed/Nulls are found","Null values found in":output1.tolist()}
+				else:
+					line1 = {"Test name": "Check for nulls", "Result": "Passed"}
+
+				# column names validation
+
+				l1 = (list(client_file_data.columns))
+				l2 = (list(json_def_data["columns"].index))
+
+				if len(set(l1).intersection(l2)) == len(l2) :
+					line2 = {"Test name": "Column names", "Result": "Passed"}
+				else:
+					line2 = {"Test name": "Column names", "Result": "Failed", "Output": "The partner file has other columns "+str(set(l1).union(l2) - set(l1).intersection(l2))}
+
+				# column order validation
+
+				l2 = (json_def_data["columns"])
+				l2_values = (list(json_def_data["columns"].index))
+
+				temp1 = []
+				def_col, pass_list, fail_list = {}, {}, {}
+
+				for index, col in enumerate(l2_values):
+					if int(l2[col]['order']) <= len(l1):
+						def_col[int(l2[col]['order'])-1] = col
+						def_col.update(def_col)
+				if len(def_col.keys()) != len(l1):
+					fail_list[-1000] = "Length of the columns doesn't match and the columns present in partner files are"
+					for i in range(len(l1)):
+						fail_list[i] = str(l1[i])
+				else:
+					for i in def_col:
+						if str(def_col[i]) == str(l1[i]):
+							pass_list[i] = str(def_col[i])
+						else:
+							fail_list[i] = str(def_col[i])
+				if len(fail_list) != 0:
+					line3 = {"Test name": "Column order", "Result": "Failed", "Output": "The expected column order is "+str(list(def_col.values()))+" but we have "+str(list(fail_list.values()))}
+				else:
+					line3 = {"Test name": "Column order", "Result": "Passed"}
+
+				# copying the file to passed or fail folder
+
+				if line1["Result"] == "Passed" and line2["Result"] == "Passed" and line3["Result"] == "Passed":
+					with open(text_file_pass+client_file_name, 'w') as f1:
+						for line in open(client_file):
+							f1.write(line)
+					Summary_line = {"FileName" : client_file_name, "TimeStamp" : today_now, "Result" : "Pass"}
+
+				else:
+					with open(text_file_fail+client_file_name, 'w') as f1:
+						for line in open(client_file):
+							f1.write(line)
+					Summary_line = {"FileName" : client_file_name, "TimeStamp" : today_now, "Result" : "Failed"}
+
+				total_Summary_line[client_file_name] = Summary_line
+
+				# writing the output to the result file
+
+				final_lines_to_file = {"Test-1" : line1, "Test-2" : line2, "Test-3" : line3}
+
+				# creating a json output file in result folder
+
+				with open(text_file_result, "w") as output:
+					json.dump(final_lines_to_file, output, indent=2)
+				output.close()
+
 			else:
-				line1 = {"Test name": "Check for nulls", "Result": "Passed"}
 
-			# column names validation
-
-			l1 = (list(client_file_data.columns))
-			l2 = (list(json_def_data["columns"].index))
-
-			if len(set(l1).intersection(l2)) == len(l2) :
-				line2 = {"Test name": "Column names", "Result": "Passed"}
-			else:
-				line2 = {"Test name": "Column names", "Result": "Failed", "Output": "The partner file has other columns "+str(set(l1).union(l2) - set(l1).intersection(l2))}
-
-			# column order validation
-
-			l2 = (json_def_data["columns"])
-			l2_values = (list(json_def_data["columns"].index))
-
-			temp1 = []
-			def_col, pass_list, fail_list = {}, {}, {}
-
-			for index, col in enumerate(l2_values):
-				if int(l2[col]['order']) <= len(l1):
-					def_col[int(l2[col]['order'])-1] = col
-					def_col.update(def_col)
-			if len(def_col.keys()) != len(l1):
-				fail_list[-1000] = "Length of the columns doesn't match and the columns present in partner files are"
-				for i in range(len(l1)):
-					fail_list[i] = str(l1[i])
-			else:
-				for i in def_col:
-					if str(def_col[i]) == str(l1[i]):
-						pass_list[i] = str(def_col[i])
-					else:
-						fail_list[i] = str(def_col[i])
-			if len(fail_list) != 0:
-				line3 = {"Test name": "Column order", "Result": "Failed", "Output": "The expected column order is "+str(list(def_col.values()))+" but we have "+str(list(fail_list.values()))}
-			else:
-				line3 = {"Test name": "Column order", "Result": "Passed"}
-
-			# copying the file to passed or fail folder
-
-			if line1["Result"] == "Passed" and line2["Result"] == "Passed" and line3["Result"] == "Passed":
-				with open(text_file_pass+client_file_name, 'w') as f1:
-					for line in open(client_file):
-						f1.write(line)
-				Summary_line = {"FileName" : client_file_name, "TimeStamp" : today_now, "Result" : "Pass"}
-
-			else:
-				with open(text_file_fail+client_file_name, 'w') as f1:
-					for line in open(client_file):
-						f1.write(line)
-				Summary_line = {"FileName" : client_file_name, "TimeStamp" : today_now, "Result" : "Failed"}
-
-			total_Summary_line[client_file_name] = Summary_line
-
-			# writing the output to the result file
-
-			final_lines_to_file = {"Test-1" : line1, "Test-2" : line2, "Test-3" : line3}
-
-			# creating a json output file in result folder
-
-			with open(text_file_result, "w") as output:
-				json.dump(final_lines_to_file, output, indent=2)
-			output.close()
+				total_Summary_line[client_file_name] = {"FileName" : client_file_name, "TimeStamp" : today_now, "Result" : "File doesn't exist in the control file"}
 
 		# writing to Summary result file
 
